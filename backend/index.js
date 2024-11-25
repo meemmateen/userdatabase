46a69d825a2788af89195e6f94df19c5ea9dcb30
meemmateen <167772740+meemmateen@users.noreply.github.com>
2024-11-25 12:50:51 +0500

    Add files via upload

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,12 @@ const mysql = require('mysql2');
 const cors = require('cors');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: 'https://userdatabase-five.vercel.app' 
+}));
 app.use(express.json());
 
 // MySQL Connection
@@ -90,5 +92,5 @@ app.delete('/api/data/:id', (req, res) => {
 
 // Start Server
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
